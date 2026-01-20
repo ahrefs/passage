41fd28012ad0b4a5e617f8784b019e5fc6a9c5cd
José Nogueira <ze@thatportugueseguy.com>
2026-01-20 14:47:03 +0000

    remove base module

--- lib/base.mli
+++
@@ -1,4 +0,0 @@
-val printfn : ('a, unit, string, unit) format4 -> 'a
-val eprintfn : ('a, unit, string, unit) format4 -> 'a
-val verbose_eprintlf : ?verbose:bool -> ('a, unit, string, unit) format4 -> 'a
-val die : ?exn:exn -> ('a, unit, string, 'b) format4 -> 'a
